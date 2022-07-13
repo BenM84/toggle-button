aa7ff4d609051e8d605a9705be3c5cf7df3ee0a0
BenM84 <96224617+BenM84@users.noreply.github.com>
2022-07-13 23:04:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, {useState} from "react";
+import "./App.css";
+import {Switch} from "antd";
+
+
+const App = () => {
+
+  const [toggle, setToggle] = useState(false);
+
+  const toggler = () => {
+    toggle ? setToggle(false) : setToggle(true);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Switch onClick={toggler}/>
+     {toggle ? <span>Well darn it ya change</span>: <span>Wow still the same old you</span>}
     </div>
   );
 }
+
 
 export default App;
